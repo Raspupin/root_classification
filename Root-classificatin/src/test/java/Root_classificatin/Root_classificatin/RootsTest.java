e101a8548627d22560d39c4a581dc2dd7091fc94
Daniel Ermakov <danielermakov2012@gmail.com>
2024-02-01 17:38:35 +0200

    v3 added the test functions

--- Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
+++ Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
@@ -1,14 +1,17 @@
 package Root_classificatin.Root_classificatin;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
+
+import grade_for_chars.App;
 
 public class RootsTest 
 {
 	Roots root;
 
 	@Test
-	public void testPartion1() 
+	public void testPartion1A() 
 	{
 		//(0,1,2)
 		Roots.calculate_roots(-2, 1, 2);
@@ -18,5 +21,29 @@ public class RootsTest
 		
 		assertEquals(root_expected, Roots.root_types().toString());
 	}
+	@Test
+    public void testPartion1AAtMinimumBoundary() {
+       
+    }
+
+    @Test
+    public void testPartion1AJustAboveMinimumBoundary() {
+        assertEquals("D", App.returnAGrade(61), "Grade just above the minimum boundary should be D");
+    }
+
+    @Test
+    public void testPartion1AAtNominalValue() {
+        assertEquals("D", App.returnAGrade(65), "Grade at the nominal value should be D");
+    }
+
+    @Test
+    public void testPartion1AJustBelowMaximumBoundary() {
+        assertEquals("D", App.returnAGrade(68), "Grade just below the maximum boundary should be D");
+    }
+
+    @Test
+    public void ttestPartion1AAtMaximumBoundary() {
+        assertEquals("D", App.returnAGrade(69), "Grade at the maximum boundary should be D");
+    }
 	
-	}
+}
