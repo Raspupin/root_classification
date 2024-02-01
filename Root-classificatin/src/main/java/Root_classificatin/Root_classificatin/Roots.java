4d55db2cc2af3a4e9279c1fa9eb969ad8c801b57
Daniel Ermakov <danielermakov2012@gmail.com>
2024-02-01 17:13:34 +0200

    v2.1 fixed wrong enum outputs for if statments.

--- Root-classificatin/src/main/java/Root_classificatin/Root_classificatin/Roots.java
+++ Root-classificatin/src/main/java/Root_classificatin/Root_classificatin/Roots.java
@@ -50,12 +50,12 @@ class Roots
 	      {
 		      // the equation has no roots if b^2 < 4ac
 		     
-		      roots = Root_Types.Not_quadratic;
+		      roots = Root_Types.No_roots;
 	      }
       }
       else {
     	  
-    	  roots = Root_Types.Two_roots; 
+    	  roots = Root_Types.Not_quadratic; 
       }
    }
 
