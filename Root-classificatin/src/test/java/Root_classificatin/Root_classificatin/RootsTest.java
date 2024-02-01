3087c1baa962fadbc7224da05704276fe0d8514b
Daniel Ermakov <danielermakov2012@gmail.com>
2024-02-01 17:52:37 +0200

    v3.1 completed all the test cases.

--- Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
+++ Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
@@ -1,49 +1,61 @@
 package Root_classificatin.Root_classificatin;
 import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.Test;
 
-import grade_for_chars.App;
 
 public class RootsTest 
 {
 	Roots root;
 
+
 	@Test
-	public void testPartion1A() 
-	{
-		//(0,1,2)
-		Roots.calculate_roots(-2, 1, 2);
+    public void testPartion1AAtMinimumBoundary() {
+		Roots.calculate_roots(0, 2, 2);
+		
+		String  root_expected = Root_Types.Not_quadratic.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+    }
+
+    @Test
+    public void testPartion1AJustAboveMinimumBoundary() {
+    	Roots.calculate_roots(1, 2, 10);
+		
+		String  root_expected = Root_Types.No_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+    }
+
+    @Test
+    public void testPartion1ANominalValue() {
+    	Roots.calculate_roots(40, 80, 1);
+		
+		String  root_expected = Root_Types.Equal_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+    }
+
+    @Test
+    public void testPartion1AJustBelowMaximumBoundary() {
+    	Roots.calculate_roots(99, 2, -1);
+		
+		String  root_expected = Root_Types.Two_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+    }
+
+    @Test
+    public void ttestPartion1AAtMaximumBoundary() {
+    	Roots.calculate_roots(100, 200, 2);
 		
 		String  root_expected = Root_Types.ERROR.toString();
 		
 		
 		assertEquals(root_expected, Roots.root_types().toString());
-	}
-	@Test
-    public void testPartion1AAtMinimumBoundary() {
-       
-    }
-
-    @Test
-    public void testPartion1AJustAboveMinimumBoundary() {
-        assertEquals("D", App.returnAGrade(61), "Grade just above the minimum boundary should be D");
-    }
-
-    @Test
-    public void testPartion1AAtNominalValue() {
-        assertEquals("D", App.returnAGrade(65), "Grade at the nominal value should be D");
-    }
-
-    @Test
-    public void testPartion1AJustBelowMaximumBoundary() {
-        assertEquals("D", App.returnAGrade(68), "Grade just below the maximum boundary should be D");
-    }
-
-    @Test
-    public void ttestPartion1AAtMaximumBoundary() {
-        assertEquals("D", App.returnAGrade(69), "Grade at the maximum boundary should be D");
     }
 	
 }
